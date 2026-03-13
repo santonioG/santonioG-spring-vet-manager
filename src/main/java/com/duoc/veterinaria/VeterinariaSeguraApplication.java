a4ba69df923efbb786b6543b3be047070a4ced82
santonioG <205916771+santonioG@users.noreply.github.com>
2026-03-12 22:52:27 -0300

    Initial commit: Spring Boot skeleton with Security and Thymeleaf

---
+++ src/main/java/com/duoc/veterinaria/VeterinariaSeguraApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaSeguraApplication.class, args);
+	}
+
+}
