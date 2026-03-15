e2794b8db8ef5d1c7338b5d4ac4aff915eecb639
santonioG <205916771+santonioG@users.noreply.github.com>
2026-03-14 21:10:25 -0300

    Sistema funcional: login y gestión de pacientes operativos

--- src/main/java/com/duoc/veterinaria/controller/PacienteController.java
+++ src/main/java/com/duoc/veterinaria/controller/PacienteController.java
@@ -1,18 +1,14 @@
 package com.duoc.veterinaria.controller;
 
-public class PacienteController {
-
-}
-package com.duoc.veterinaria.controller;
-
-import com.duoc.veterinaria.model.Paciente;
-import com.duoc.veterinaria.service.IPacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.duoc.veterinaria.model.Paciente;
+import com.duoc.veterinaria.service.IPacienteService;
 
 @Controller
 public class PacienteController {
